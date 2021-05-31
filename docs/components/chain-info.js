d34a7b66100e2009f7d12ff0c249c11c6f51934a
DavidWork <davidwork@DavidWorks-MacBook-Pro.local>
2021-05-30 23:01:33 -0700

    updated dropdown menus with chain names

---
+++ docs/components/chain-info.js
@@ -0,0 +1,14 @@
+export const allChainData = new Map([
+    [1, {dbname: "chain_eth_mainnet", chain_name: "Ethereum Mainnet"}],
+    [42, {dbname: "chain_eth_kovan", chain_name: "Ethereum Testnet Kovan"}],
+    [137, {dbname: "chain_matic_mainnet", chain_name: "Matic Mainnet"}],
+    [80001, {dbname: "chain_matic_mumbai", chain_name: "Matic Testnet Mumbai"}],
+    [43114, {dbname: "chain_avalanche_mainnet", chain_name: "Avalanche C-Chain Mainnet"}],
+    [43113, {dbname: "chain_avalanche_testnet", chain_name: "Avalanche Fuji Testnet"}],
+    [56, {dbname: "chain_bsc", chain_name: "Binance Smart Chain"}],
+    [97, {dbname: "chain_bsc_testnet", chain_name: "Binance Smart Chain Testnet"}],
+    [250, {dbname: "chain_fantom_mainnet", chain_name: "Fantom Opera"}],
+    [1131378225, {dbname: "chain_covint1", chain_name: "Covalent Internal V1"}],
+    [1287, {dbname: "chain_moonbeam_moonbase_alpha", chain_name: "Moonbeam Moonbase Alpha"}]
+]);
+    
--- docs/components/get-log-data/get-log-data.js
+++ docs/components/get-log-data/get-log-data.js
@@ -1,18 +1,44 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "../../css/components/topic-calculator.css";
 import GetContractLogs from "./get-contract-logs";
 import GetTopicHashLogs from "./log-events-topic-hash";
+import { allChainData } from '../chain-info';
 import "./accordion.css";
 
 const GetLogs = () => {
 
-  const [setActive, setActiveState] = useState(" ");
+  const [setActive, setActiveState] = useState("");
+  const [setOverflow, setOverflowState] = useState("hidden");
   const [setHeight, setHeightState] = useState("0px");
+  const [chainData, setChainData] = useState([]);
 
   const content = useRef(null);
 
+  const temp = [];
+
+  useEffect(() => {
+    fetchAllChainsData()
+  }, []);
+
+  const fetchAllChainsData = async () => {
+    const response = await fetch("https://api.covalenthq.com/v1/chains/?key=ckey_6b87a4a549ff46e6971c3e6341f")
+    if (response.ok) {
+      const data = await response.json()
+      for (var i = 0; i < data.data.items.length; i++) {
+        if (allChainData.has(Number(data.data.items[i].chain_id))) {
+          temp[i] = {
+            value: data.data.items[i].chain_id,
+            label: allChainData.get(Number(data.data.items[i].chain_id)).chain_name
+          }
+        }  
+      }
+      setChainData(temp);
+    } 
+  }
+
 
   function toggleAccordion() {
+    setOverflowState(setOverflow === "hidden" ? "" : "hidden");
     setActiveState(setActive === "" ? "active" : "");
     setHeightState(
       setActive === "active" ? "0px" : `${content.current.scrollHeight}px`
@@ -21,20 +47,18 @@ const GetLogs = () => {
   
   return (
     <div>
-      <div className="topics">
-   <GetContractLogs />
+      {chainData.length !== 0 ? <div className="topics">
+        <GetContractLogs chains={chainData}/>
 
-   <div className="accordion__section">
-     <button className={`accordion ${setActive}`} onClick={toggleAccordion}>
-       <p className="accordion__title">Get Log Events by Topic Hashes</p>
-     </button>
-     <div ref={content} style={{ maxHeight: `${setHeight}` }} className="accordion__content">
-     <GetTopicHashLogs />
-     </div>
-   </div>
-
-       
+      <div className="accordion__section">
+        <button className={`accordion ${setActive}`} onClick={toggleAccordion}>
+          <p className="accordion__title">Get Log Events by Topic Hashes</p>
+        </button>
+        <div ref={content} style={{ maxHeight: `${setHeight}`, overflow: `${setOverflow}`, transition: `${setOverflow === "hidden" ? "max-height 0.6s ease" : "overflow 0s linear 0s"}`}} className="accordion__content">
+          <GetTopicHashLogs chains={chainData} />
+        </div>
       </div>
+      </div> : null }
     </div>
   );
 };
